d8e41eb3ba3d205f47abc2b39e0c5e03c8cb5c40
Avrohom Yisroel Silver <mryossu@hotmail.com>
2023-07-05 15:15:48 +0100

    Added a migration to make sure it all still works after all the refactoring I just did

--- IdentityClaimsPlay.Data/Data/Company.cs
+++ IdentityClaimsPlay.Data/Data/Company.cs
@@ -4,6 +4,7 @@ namespace IdentityClaimsPlay.Data.Data;
 
 public class Company : EntityBase {
   public string Name { get; set; } = "";
+  public string Address { get; set; } = "";
   public string Domain { get; set; } = "";
   public virtual ObservableCollection<Charity> Charities { get; set; } = new();
 }
---
+++ IdentityClaimsPlay.Data/Migrations/20230705141344_AddedAddressToCompany.cs
@@ -0,0 +1,21 @@
+﻿#nullable disable
+
+namespace IdentityClaimsPlay.Data.Migrations {
+  /// <inheritdoc />
+  public partial class AddedAddressToCompany : Migration {
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder) =>
+      migrationBuilder.AddColumn<string>(
+        name: "Address",
+        table: "Companies",
+        type: "nvarchar(max)",
+        nullable: false,
+        defaultValue: "");
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) =>
+      migrationBuilder.DropColumn(
+        name: "Address",
+        table: "Companies");
+  }
+}
